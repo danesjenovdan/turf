d18e705a8e978a5db3375ba38915461b0f6ccec2
Filip Muki Dobranic <filipdobranic@gmail.com>
2018-07-14 17:32:39 +0200

    make parse.sh configurable

--- parse.sh
+++ parse.sh
@@ -1,1 +1,6 @@
-scrapy runspider rtvslo.py -o comments/rtvslo_$(date -d "today" +"%Y%m%d%H%M").json -loglevel=ERROR
+SCRAPY_BIN=/home/user/.virtualenvs/turf/bin/scrapy
+SCRIPT_PATH=/home/user/turf/rtvslo.py
+COMMENTS_FOLDER=/home/user/turf/comments
+LOG_FOLDER=/home/user/turf/log
+
+$SCRAPY_BIN runspider $SCRIPT_PATH -o $COMMENTS_FOLDER/rtvslo_$(date -d "today" +"%Y%m%d%H%M").json --loglevel=ERROR >> $LOG_FOLDER/turfer.log
